1032b2e346039b2d39c97f7278a89ae40735eda5
Ariel Kenan <ArielDKenan@gmail.com>
2016-02-23 17:40:13 +0200

    'message'

--- js/index.js
+++ js/index.js
@@ -5,26 +5,26 @@ $(function(){
    $('.heart').on('mouseover', function(){
        $('.heart').hide();
        $('.redheart').show();
-   })
+   });
 });
 
 $(function(){
     $('.redheart').on('mouseout', function(){
         $('.redheart').hide();
         $('.heart').show();
-    })
+    });
 });
 //959494
 $(function(){
    $('.play-btn').on('click', function(){
        $('.play-btn').hide();
        $('.pause-btn').show();
-   })
+   });
 });
 
 $(function(){
    $('.pause-btn').on('click', function(){
        $('.pause-btn').hide();
        $('.play-btn').show();
-   })
+   });
 });
--- js/player.js
+++ js/player.js
@@ -10,11 +10,10 @@ player.init = function () {
     SC.initialize({
         client_id: '27bcac07db1cde6ee2ff5f3ad8d79969'
     });
-    console.log('jquery init');
     $('.play-btn').on('click', function () {
         $("#audio-test")[0].play();
     });
-    $('.next-btn').on('click', player.playSong);
+    $('.skip-btn').on('click', player.playSong);
     $('.redheart').on('click', client.likeSong);
     $('.pause-btn').on('click', function () {
         $("#audio-test")[0].pause();
@@ -27,11 +26,14 @@ player.playSong = function () {
         player.songid = sobj.song_id;
         console.log(sobj.song_id);
         player.song = sobj;
+        $('.title').text(sobj.song_title);
+        $('.artist').text(sobj.user_name);
         SC.get("/tracks/" + player.songid).then(function(sound){
             console.log(sound.stream_url);
             var url = sound.stream_url + "?client_id=27bcac07db1cde6ee2ff5f3ad8d79969";
             $("#audio-test").attr("src", url);
             $("#audio-test")[0].play();
+            client.liked = false;
         });
     });
 };
